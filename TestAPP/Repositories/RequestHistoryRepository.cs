77951f5751c1f027b63e4c176110cd963e10076e
Tisavaco <40355964+Tisavaco@users.noreply.github.com>
2023-12-14 13:35:47 +0300

    Add files via upload

---
+++ TestAPP/Repositories/RequestHistoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestAPP.Converters;
+using TestAPP.Entity;
+
+namespace TestAPP.Repositories
+{
+    public class RequestHistoryRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public RequestHistoryRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task WriteData(IpInfo ipInfo)
+        {
+            var requestHistory = MyConverter.ConvertToRequestHistory(ipInfo);
+            if (!await appDbContext.RequestHistorys.AnyAsync(x => x.IPAddress == requestHistory.IPAddress))
+            {
+                appDbContext.Add(MyConverter.ConvertToRequestHistory(ipInfo));
+                await appDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
